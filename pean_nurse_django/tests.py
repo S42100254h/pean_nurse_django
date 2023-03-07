5fcefeaa515279f7647142bb5cfd42b668031c18
yuki sawabe <sawabe0v0@gmail.com>
2023-03-07 20:38:38 +0900

    test for create method

--- pean_nurse_django/tests.py
+++ pean_nurse_django/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from .models import User
 
-# Create your tests here.
+
+class UserTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.model = User.objects.create(name='dummy', email='dummy@example.com')
+
+    def test_create(self):
+        data = {'name': 'neko', 'email': 'neko@gmail.com'}
+        url = reverse('user-list')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 2)
+        self.assertEqual(User.objects.get(id=response.data['id']).name, 'neko')
+        
