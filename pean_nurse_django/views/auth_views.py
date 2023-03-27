5b2e594bcf4c1eb1a68d18f7d1f20a494fc98265
yuki sawabe <sawabe0v0@gmail.com>
2023-03-27 15:16:58 +0900

    create view for login

---
+++ pean_nurse_django/views/auth_views.py
@@ -0,0 +1,21 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
+from django.contrib.auth import authenticate, login
+
+
+class AuthView(APIView):
+    authentication_classes = [TokenAuthentication]
+
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key})
+        else:
+            return Response({'error': 'Invalid Credentials'})
