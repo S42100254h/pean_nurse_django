cda83ba9e0d8de2082905db00af9de38febc068e
yuki sawabe <sawabe0v0@gmail.com>
2023-03-27 13:46:20 +0900

    update directory structure for views

--- pean_nurse_django/views/user_views.py
+++ pean_nurse_django/views/user_views.py
@@ -1,8 +1,8 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-from .models import User
-from .serializer import UserSerializer
+from ..models import User
+from ..serializer import UserSerializer
 
 
 class UserViewSet(viewsets.ViewSet):
