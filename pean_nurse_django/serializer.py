28ff57c71f8d24d0d8fe301717d14021611c7e11
yuki sawabe <sawabe0v0@gmail.com>
2023-03-27 16:28:37 +0900

    fix UserSerializer

--- pean_nurse_django/serializer.py
+++ pean_nurse_django/serializer.py
@@ -5,4 +5,12 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'name', 'email')
+        fields = ('id', 'name', 'email', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            name = validated_data['name'],
+            email = validated_data['email'],
+            password = validated_data['password'],
+        )
+        return user
